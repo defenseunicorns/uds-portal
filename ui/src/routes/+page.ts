343b943aeba683d62d8fe691cade23f858aa1489
Joel McCoy <joel@defenseunicorns.com>
2026-05-18 14:32:25 -0500

    chore: cleanup unused file

--- ui/src/routes/+page.ts
+++
@@ -1,5 +0,0 @@
-// Copyright 2025-2026 Defense Unicorns
-// SPDX-License-Identifier: AGPL-3.0-or-later OR LicenseRef-Defense-Unicorns-Commercial
-
-// Apps are loaded by +layout.ts and accessed via layout data (data.apps).
-export {}
